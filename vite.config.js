3206a943d3ea3efc946fa2062a566afc5bf8b5cb
Taleef Tamsal <89072337+Taleef7@users.noreply.github.com>
2024-08-05 03:18:21 +0500

    Deployment changes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Taleef7.github.io/', // Replace with your repository name
-  build: {
-    outDir: 'dist',
-  },
+  base: './', // Replace with your repository name
+  // build: {
+  //   outDir: 'dist',
+  // },
 });
