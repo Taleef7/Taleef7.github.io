1b8c6efac4c798a9883dd882f59cc04284e7ef1c
Taleef Tamsal <89072337+Taleef7@users.noreply.github.com>
2024-08-05 03:00:05 +0500

    Setup for Github Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/Taleef7.github.io/', // Replace with your repository name
+  build: {
+    outDir: 'dist',
+  },
+});
